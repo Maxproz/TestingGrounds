015bb7af0e0425a818521075a1d5831d371e03f5
Maxpro <maxdietz@hotmail.com>
2016-11-26 19:05:25 -0800

    Setup the tile to generate in a for loop for our terrain

--- Source/TestingGrounds/Player/CharacterV2.cpp
+++ Source/TestingGrounds/Player/CharacterV2.cpp
@@ -75,6 +75,8 @@ void ACharacterV2::BeginPlay()
     }
     
     Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+    
+    // TODO: Figure out how to use the gun fire action binding without using the Mesh1p without using this mesh or find another way to do these actions.
     Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
     Gun->AnimInstance = Mesh1P->GetAnimInstance();
 }
